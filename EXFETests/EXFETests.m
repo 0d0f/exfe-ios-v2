3ab9000149892845c0bf978fd0b3435a95076893
Stony Wang <stonyw@gmail.com>
2013-01-22 12:09:13 +0800

    add unit test target

---
+++ EXFETests/EXFETests.m
@@ -0,0 +1,34 @@
+//
+//  EXFETests.m
+//  EXFETests
+//
+//  Created by Stony Wang on 13-1-22.
+//
+//
+
+#import "EXFETests.h"
+
+@implementation EXFETests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertTrue(YES, @"ok");
+    
+    //STFail(@"Unit tests are not implemented yet in EXFETests");
+}
+
+@end
