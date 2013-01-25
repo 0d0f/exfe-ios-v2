f43820c630f8eeceebf8c56cb8979305299a5fe0
Stony Wang <stonyw@gmail.com>
2013-01-25 15:16:54 +0800

    add header for profile screen

--- EXFE/CustomAttributedTextView.m
+++ EXFE/CustomAttributedTextView.m
@@ -29,22 +29,25 @@
 - (void)drawRect:(CGRect)rect
 {
 //    [text drawInRect:rect];
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGContextSaveGState(context);
-    
-    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
-    CGContextTranslateCTM(context, 0, self.bounds.size.height);
-    CGContextScaleCTM(context, 1.0, -1.0);
-    CGContextSetShadowWithColor(context, CGSizeMake(0, 1.0f), 1.0f, [UIColor blackColor].CGColor);
-
-    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)text);
-    CGMutablePathRef path = CGPathCreateMutable();
-    CGPathAddRect(path, NULL, rect);
-    CTFrameRef theFrame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, [text length]), path, NULL);
-    CFRelease(framesetter);
-    CFRelease(path);
-    CTFrameDraw(theFrame, context);
-    CFRelease(theFrame);
+    if (text != nil) {
+        
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        CGContextSaveGState(context);
+        
+        CGContextSetTextMatrix(context, CGAffineTransformIdentity);
+        CGContextTranslateCTM(context, 0, self.bounds.size.height);
+        CGContextScaleCTM(context, 1.0, -1.0);
+        CGContextSetShadowWithColor(context, CGSizeMake(0, 1.0f), 1.0f, [UIColor blackColor].CGColor);
+        
+        CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)text);
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathAddRect(path, NULL, rect);
+        CTFrameRef theFrame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, [text length]), path, NULL);
+        CFRelease(framesetter);
+        CFRelease(path);
+        CTFrameDraw(theFrame, context);
+        CFRelease(theFrame);
+    }
     
 }
 
