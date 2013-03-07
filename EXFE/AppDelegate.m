a1a0a585c07c814f86f918af9497a8fb78692da9
huo ju <virushuo@gmail.com>
2013-03-07 17:36:33 +0800

    mapping exfee

--- EXFE/AppDelegate.m
+++ EXFE/AppDelegate.m
@@ -67,6 +67,21 @@ static char handleurlobject;
   identityMapping.identificationAttributes = @[ @"identity_id" ];
   [identityMapping addAttributeMappingsFromDictionary:@{@"id": @"identity_id",@"order": @"a_order"}];
   [identityMapping addAttributeMappingsFromArray:@[@"name",@"nickname",@"provider",@"external_id",@"external_username",@"connected_user_id",@"bio",@"avatar_filename",@"avatar_updated_at",@"created_at",@"updated_at",@"type",@"unreachable",@"status"]];
+
+  RKEntityMapping *invitationMapping = [RKEntityMapping mappingForEntityForName:@"Invitation" inManagedObjectStore:managedObjectStore];
+  invitationMapping.identificationAttributes = @[ @"invitation_id" ];
+  [invitationMapping addAttributeMappingsFromDictionary:@{@"id": @"invitation_id"}];
+  [invitationMapping addAttributeMappingsFromArray:@[@"rsvp_status",@"host",@"mates",@"via",@"updated_at",@"created_at",@"type"]];
+
+  [invitationMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"identity" toKeyPath:@"identity" withMapping:identityMapping]];
+  [invitationMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"invited_by" toKeyPath:@"invited_by" withMapping:identityMapping]];
+  [invitationMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"updated_by" toKeyPath:@"updated_by" withMapping:identityMapping]];
+  
+  RKEntityMapping *exfeeMapping = [RKEntityMapping mappingForEntityForName:@"Exfee" inManagedObjectStore:managedObjectStore];
+  exfeeMapping.identificationAttributes = @[ @"exfee_id" ];
+  [exfeeMapping addAttributeMappingsFromDictionary:@{@"id": @"exfee_id"}];
+  [exfeeMapping addAttributeMappingsFromArray:@[@"total",@"accepted",@"type"]];
+  [exfeeMapping addPropertyMapping:[RKRelationshipMapping relationshipMappingFromKeyPath:@"invitations" toKeyPath:@"invitations" withMapping:invitationMapping]];
   
   RKEntityMapping *userMapping = [RKEntityMapping mappingForEntityForName:@"User" inManagedObjectStore:managedObjectStore];
   userMapping.identificationAttributes = @[ @"user_id" ];
@@ -81,17 +96,6 @@ static char handleurlobject;
   [objectManager addResponseDescriptor:responseDescriptor];
   
 
-  RKEntityMapping *exfeeMapping = [RKEntityMapping mappingForEntityForName:@"Exfee" inManagedObjectStore:managedObjectStore];
-  exfeeMapping.identificationAttributes = @[ @"exfee_id" ];
-  [exfeeMapping addAttributeMappingsFromDictionary:@{@"id": @"exfee_id"}];
-  [exfeeMapping addAttributeMappingsFromArray:@[@"total",@"accepted",@"type"]];
-  
-  
-  //    exfeeMapping.primaryKeyAttribute=@"exfee_id";
-  //    [exfeeMapping mapKeyPathsToAttributes:@"total",@"total",@"accepted",@"accepted",@"type",@"type",
-  //     nil];
-  //    [exfeeMapping mapRelationship:@"invitations" withMapping:invitationMapping];
-  //    return exfeeMapping;
 
   
   RKEntityMapping *crossMapping = [RKEntityMapping mappingForEntityForName:@"Cross" inManagedObjectStore:managedObjectStore];
