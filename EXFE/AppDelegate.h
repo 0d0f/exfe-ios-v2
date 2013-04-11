9fbc340da8e17f899e6ddadc07c5b4f7d0e47346
Stony Wang <stonyw@gmail.com>
2013-04-12 02:09:51 +0800

    add more targets

--- EXFE/AppDelegate.h
+++ EXFE/AppDelegate.h
@@ -18,36 +18,32 @@
 
 
 #ifdef DEBUG
-#define DEV
+#ifndef SERVER
+#define SERVER  DEV
+#endif
 #else
-#define WWW
+#define SERVER  WWW
 #endif
 
-#ifdef DEV
+#if SERVER == DEV
 #define API_SERVER @"http://api.0d0f.com/"
 #define API_ROOT @"http://api.0d0f.com/v2"
 #define IMG_ROOT @"http://0d0f.com/static/img"
 #define EXFE_OAUTH_LINK @"http://0d0f.com/OAuth"
 #define GOOGLE_API_KEY @"AIzaSyDTc7JJomGg5SW7Zn7lTN0N6mqAI9T3tFg"
-#endif
-
-#ifdef PANDA
+#elif SERVER == PANDA
 #define API_SERVER @"http://api.panda.0d0f.com/"
 #define API_ROOT @"http://api.panda.0d0f.com/v2"
 #define IMG_ROOT @"http://panda.0d0f.com/static/img"
 #define EXFE_OAUTH_LINK @"http://panda.0d0f.com/oauth"
 #define GOOGLE_API_KEY @"AIzaSyDTc7JJomGg5SW7Zn7lTN0N6mqAI9T3tFg"
-#endif
-
-#ifdef LOCAL
+#elif SERVER == LOCAL
 #define API_SERVER @"http://api.local.exfe.com/"
 #define API_ROOT @"http://api.local.exfe.com/v2"
 #define IMG_ROOT @"http://local.exfe.com/static/img"
 #define EXFE_OAUTH_LINK @"http://local.exfe.com/OAuth"
 #define GOOGLE_API_KEY @"AIzaSyDTc7JJomGg5SW7Zn7lTN0N6mqAI9T3tFg"
-#endif
-
-#ifdef WWW
+##elif SERVER == WWW
 #define API_SERVER @"https://www.exfe.com/"
 #define API_ROOT @"https://www.exfe.com/v2"
 #define IMG_ROOT @"https://exfe.com/static/img"
