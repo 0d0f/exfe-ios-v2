247876c61ba1a02bfa5760bd596e5871de1e6949
Stony Wang <stonyw@gmail.com>
2013-04-17 02:25:33 +0800

    add pwd field

---
+++ EXFE/EFPasswordField.h
@@ -0,0 +1,13 @@
+//
+//  EFPasswordField.h
+//  EXFE
+//
+//  Created by Stony Wang on 13-4-16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EFPasswordField : UITextField
+
+@end
---
+++ EXFE/EFPasswordField.m
@@ -0,0 +1,31 @@
+//
+//  EFPasswordField.m
+//  EXFE
+//
+//  Created by Stony Wang on 13-4-16.
+//
+//
+
+#import "EFPasswordField.h"
+
+@implementation EFPasswordField
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
