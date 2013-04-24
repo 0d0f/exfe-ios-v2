ce7b4dc9c5dd1cee39be0b749c12068f1c984ffb
0day2010 <0day.zh@gmail.com>
2013-04-24 15:27:06 +0800

    Limit provider icon top to 3 && Add SearchIdentityCell

---
+++ EXFE/EFSearchIdentityCell.h
@@ -0,0 +1,18 @@
+//
+//  EFSearchIdentityCell.h
+//  EXFE
+//
+//  Created by 0day on 13-4-24.
+//
+//
+
+#import "EFChoosePeopleViewCell.h"
+#import "Util.h"
+
+@interface EFSearchIdentityCell : EFChoosePeopleViewCell
+
++ (NSString *)reuseIdentifier;
+
+- (void)customWithIdentityString:(NSString *)string candidateProvider:(Provider)candidateProvider matchProvider:(Provider)matchProvider;
+
+@end
