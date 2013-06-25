ac3382fbd872d2dc4d714617a7ad1eaf06e339a9
Stony Wang <stonyw@gmail.com>
2013-06-25 18:38:41 +0800

    publish support 'wc'when ssh user is not same with stony

--- publish.sh
+++ publish.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 PROJECT="$1"
-SERVER="wangchen@app.0d0f.com"
+SERVER="app.0d0f.com"
+if [ -n "$EXFE_USER" ]
+  then
+    echo "if then"
+    SERVER=$EXFE_USER"@"$SERVER
+fi
 EXT=".ipa"
 IPA_PATH=`pwd`"/builds/$PROJECT$EXT"
-SEED=$(date '+_%y%m%d')
+SEED=$(date '+_%y%m%d_%H%M%S')
 BUILD=$PROJECT$SEED$EXT
-if [ -f $build ]
-then
-    SEED=$(date '+_%y%m%d_%H%M%S')
-    BUILD=$PROJECT$SEED$EXT
-fi
-echo $PROJECT $IPA_PATH $BUILD
+echo $PROJECT $IPA_PATH $SERVER:/0d0f/app/ios/$BUILD
 chmod 666 $IPA_PATH
-#scp $IPA_PATH $SERVER:/usr/local/www/exfeweb/static/img/
-#scp $IPA_PATH $SERVER:/usr/home/stony/beta/iOS/$BUILD
 scp $IPA_PATH $SERVER:/0d0f/app/ios
 scp $IPA_PATH $SERVER:/0d0f/app/ios/$BUILD
