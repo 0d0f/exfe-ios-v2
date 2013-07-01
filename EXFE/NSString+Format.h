e3f84ae7718825c15cfc39611c27f6b08fe07e2b
0day2010 <0day.zh@gmail.com>
2013-07-01 16:58:08 +0800

    ADD NSString Format Category

---
+++ EXFE/NSString+Format.h
@@ -0,0 +1,15 @@
+//
+//  NSString+Format.h
+//  EXFE
+//
+//  Created by 0day on 13-7-1.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Format)
+
+- (NSString *)stringWithoutSpace;
+
+@end
