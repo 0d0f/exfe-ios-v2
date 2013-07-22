ea81996ddca362df623c9196b34d484c2119234a
Stony Wang <stonyw@gmail.com>
2013-07-22 16:39:55 +0800

    add view mode(readonly)

--- EXFE/EFAPIServer.m
+++ EXFE/EFAPIServer.m
@@ -1069,7 +1069,7 @@
                                                                   }];
     AFHTTPRequestOperation *operation = [objectManager.HTTPClient HTTPRequestOperationWithRequest:request success:success failure:failure];
     [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
-        NSLog(@"Sent %lld of %lld bytes", totalBytesWritten, totalBytesExpectedToWrite);
+//        NSLog(@"Sent %lld of %lld bytes", totalBytesWritten, totalBytesExpectedToWrite);
     }];
     [objectManager.HTTPClient.operationQueue addOperation:operation];
 }
